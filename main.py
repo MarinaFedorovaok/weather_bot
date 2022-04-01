0cb99aea752977a3c499a6b3e079baabbafce400
MarinaFedorovaok <fedma001@mail.ru>
2022-04-02 00:09:24 +0300

    started weather_bot

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+
+params = {
+    'latitude': '52.52',
+    'longitude': '13.41',
+    'hourly': 'temperature_2m',
+}
+
+response = requests.get('https://api.open-meteo.com/v1/forecast', params=params)
+data = response.json()
+
+print(data)
